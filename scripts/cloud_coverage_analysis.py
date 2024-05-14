28a760e7ee02bee427dc7ae16de700c42dc5e39b
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2024-05-14 14:34:35 -0700

    add cloud coverage analysis script

---
+++ scripts/cloud_coverage_analysis.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os
+import click
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+import rasterio as rio
+from pathlib import Path
+
+
+def find_matching_mask(path):
+    head, tail = os.path.split(path)
+    prefix = '_'.join(tail.split('_')[:2])
+    candidates = glob(os.path.join(head, f'{prefix}*udm2*.tif'))
+    if len(candidates) != 1:
+        return None
+    return candidates[0]
+
+
+def get_clear_pc(path):
+    with rio.open(path) as data:
+        clear = data.read(1)
+        return np.average(clear)
+
+
+@click.command()
+@click.argument('inputdir', type=click.Path(
+    path_type=Path, exists=True
+))
+@click.argument('outputfile', type=click.Path(
+    path_type=Path, exists=False
+))
+def main(inputdir, outputfile):
+
+    rgb_files = sorted(glob(os.path.join(inputdir, '*rgb.tif')))
+    mask_files = list(map(find_matching_mask, rgb_files))
+
+    data = []
+    pairs = list(zip(rgb_files, mask_files))
+    for rgbf, maskf in tqdm(pairs, 'Scanning'):
+        pc = get_clear_pc(maskf)
+        base = os.path.splitext(os.path.basename(rgbf))[0]
+        data.append((base, pc))
+
+    with open(outputfile, 'w') as f:
+        f.write('File,PercentClear\n')
+        for base, pc in data:
+            f.write(f'{base},{pc}\n')
+
+
+if __name__ == '__main__':
+    main()
