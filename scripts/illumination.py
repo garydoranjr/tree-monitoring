dd1e5ee0037d75a50fa1f91ec1094f5d40dab126
Gary Doran <Gary.B.Doran.Jr@jpl.nasa.gov>
2025-04-15 22:40:29 -0700

    update illumination script to average across years

--- scripts/illumination.py
+++ scripts/illumination.py
@@ -2,7 +2,10 @@
 import click
 import numpy as np
 import pandas as pd
+from datetime import timedelta, timezone
 import matplotlib.pyplot as plt
+from pysolar.solar import get_altitude
+from pysolar.radiation import get_radiation_direct
 
 from calc_decid_resolution import (
     CadenceInterp
@@ -19,28 +22,70 @@ def select(df, start_str, end_str):
 
 
 def select_hour(df, hour):
-    return df.loc[df['datetime'].str.endswith(hour)]
+    df['datetime'] = pd.to_datetime(
+        df['datetime'], dayfirst=True, yearfirst=False,
+    )
+    return df.loc[df['datetime'].dt.hour == hour]
 
 
 @click.command()
 @click.argument('cadencefile')
-@click.argument('minfile')
-@click.argument('meanfile')
-@click.argument('maxfile')
-def main(cadencefile, minfile, meanfile, maxfile):
+@click.argument('emeanfile')
+@click.argument('wmeanfile')
+def main(cadencefile, emeanfile, wmeanfile):
     cdata = np.load(cadencefile)
     med_idx = np.where(cdata['percentiles'] == 50.)[0][0]
     cintp = CadenceInterp(cdata['dates'], cdata['cadence'][:, med_idx])
 
-    df_min = pd.read_csv(minfile)
-    df_mean = pd.read_csv(meanfile)
-    df_max = pd.read_csv(maxfile)
+    df_mean = pd.concat([
+        pd.read_csv(emeanfile),
+        pd.read_csv(wmeanfile),
+    ], ignore_index=True)
 
-    df_min = select(df_min, '01/01/2022', '31/12/2022')
+    df_10 = select_hour(df_mean, 10)
+    daily_avg = df_10.groupby(df_10['datetime'].dt.dayofyear)['sr'].mean()
+
+    dd = np.arange(0, 365.)
+    v = cintp(dd)
+
+    latlon = (9.161750812689062, -79.83767928034351)
+
+    datetimes = pd.date_range(
+        start='2020-01-01 15:00:00',
+        periods=365,
+        freq='D',
+        tz='UTC'  # Ensures timestamps are timezone-aware
+    )
+    altitude = [
+        get_altitude(latlon[0], latlon[1], dt)
+        for dt in datetimes
+    ]
+    radiation = np.array([
+        get_radiation_direct(d, a)
+        for d, a in zip(datetimes, altitude)
+    ])
+
+    relative = daily_avg.values[:-1] / radiation
+
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(dd, daily_avg.values[:-1], 'gray')
+    #ax.plot(dd, radiation, 'r-')
+    #plt.plot(df_mean['date'], df_mean['sr'].ewm(span=30).mean())
+    ax.set_ylabel('Avg. Radiation at 10am (W/m$^2$)', fontsize=16)
+    ax2 = ax.twinx()
+    ax2.plot(dd, (1 / v), 'r')
+    ax.set_xlabel('Date', fontsize=16)
+    ax2.set_ylabel('Observations / Day', fontsize=16)
+
+    plt.show()
+
+    exit()
+
+    #df_min = select(df_min, '01/01/2022', '31/12/2022')
     df_mean = select(df_mean, '01/01/2022', '31/12/2022')
-    df_max = select(df_max, '01/01/2022', '31/12/2022')
+    #df_max = select(df_max, '01/01/2022', '31/12/2022')
 
-    df_min = select_hour(df_min, '10:00:00')
+    #df_min = select_hour(df_min, '10:00:00')
     df_mean = select_hour(df_mean, '10:00:00')
     df_max = select_hour(df_max, '10:00:00')
 
@@ -55,6 +100,7 @@ def main(cadencefile, minfile, meanfile, maxfile):
 
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(df_mean['date'], df_mean['sr'], 'gray')
+    ax.plot(df_mean['date'], radiation, 'r-')
     #plt.plot(df_mean['date'], df_mean['sr'].ewm(span=30).mean())
     ax.set_ylabel('Avg. Radiation at 10am (W/m$^2$)', fontsize=16)
     ax2 = ax.twinx()
